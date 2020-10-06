60bb4a15f7887709a5a76ce7e17e12d660e9c3ce
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-06 07:38:04 +0700

    #6 - Add UserAdapter Entity

---
+++ src/main/java/com/hendisantika/usermanagementapp/model/UserAdapter.java
@@ -0,0 +1,77 @@
+package com.hendisantika.usermanagementapp.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/10/20
+ * Time: 07.36
+ */
+public class UserAdapter implements UserDetails {
+
+    private static final long serialVersionUID = -1360188483928178893L;
+    private final User user;
+
+    public UserAdapter(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority(user.getRoleName()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return user.getId().intValue();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        UserAdapter other = (UserAdapter) obj;
+        return user.getEmail().equals(other.user.getEmail());
+    }
+
+}
