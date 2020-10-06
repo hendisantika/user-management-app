e92656e7d6fddf7127daac27163a9a57219c837b
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-07 06:57:59 +0700

    #53 - Add loadUser in InitialSetup

--- src/main/java/com/hendisantika/usermanagementapp/InitialSetup.java
+++ src/main/java/com/hendisantika/usermanagementapp/InitialSetup.java
@@ -54,4 +54,50 @@ public class InitialSetup {
         }
         loadUsers();
     }
+
+    private void loadUsers() {
+        User user1 = new User("john", "doe",
+                "john@doe.com", "123456", RoleNames.USER.name(), Boolean.TRUE);
+        userService.saveUser(user1);
+
+        User user2 = new User("Smith", "Guard",
+                "smith@gmail.com", "123456", RoleNames.USER.name(), Boolean.TRUE);
+        userService.saveUser(user2);
+
+        User user3 = new User("Lannister", "Jammy",
+                "jammy@gmail.com", "123456", RoleNames.USER.name(), Boolean.TRUE);
+        userService.saveUser(user3);
+
+        User user4 = new User("Stark", "Arya",
+                "arya@gmail.com", "123456", RoleNames.USER.name(), Boolean.TRUE);
+        userService.saveUser(user4);
+
+        User user5 = new User("Bolton", "ramsay",
+                "ramsay@gmail.com", "123456", RoleNames.USER.name(), Boolean.TRUE);
+        userService.saveUser(user5);
+
+        User user6 = new User("Clarke", "gibbins",
+                "clarke@gmail.com", "123456", RoleNames.USER.name(), Boolean.TRUE);
+        userService.saveUser(user6);
+
+        User user7 = new User("Bob", "Marley",
+                "Bob@doe.com", "123456", RoleNames.USER.name(), Boolean.TRUE);
+        userService.saveUser(user7);
+
+        User user8 = new User("Octavia", "Marley",
+                "octavia@gmail.com", "123456", RoleNames.USER.name(), Boolean.TRUE);
+        userService.saveUser(user8);
+
+        User user9 = new User("Kane", "William",
+                "Kane@gmail.com", "123456", RoleNames.USER.name(), Boolean.TRUE);
+        userService.saveUser(user9);
+
+        User user10 = new User("John", "snow",
+                "jonsnow@gmail.com", "123456", RoleNames.USER.name(), Boolean.TRUE);
+        userService.saveUser(user10);
+
+        User user11 = new User("Uzumaki", "Naruto",
+                "uzumaki_naruto@konohagakure.co.jp", "123456", RoleNames.ADMIN.name(), Boolean.TRUE);
+        userService.saveUser(user10);
+    }
 }
