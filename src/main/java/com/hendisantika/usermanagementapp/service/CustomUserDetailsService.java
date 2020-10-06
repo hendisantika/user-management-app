0c396db58dceb89e5f11018e3e9e19626abe5148
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-06 07:40:28 +0700

    #9 - Add loadUserByUsername & saveUser method in CustomUserDetailsService

--- src/main/java/com/hendisantika/usermanagementapp/service/CustomUserDetailsService.java
+++ src/main/java/com/hendisantika/usermanagementapp/service/CustomUserDetailsService.java
@@ -1,8 +1,12 @@
 package com.hendisantika.usermanagementapp.service;
 
+import com.hendisantika.usermanagementapp.model.User;
+import com.hendisantika.usermanagementapp.model.UserAdapter;
 import com.hendisantika.usermanagementapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -22,5 +26,18 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("User with %s doesn't exist!", username));
+        }
+        return new UserAdapter(user);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 }
