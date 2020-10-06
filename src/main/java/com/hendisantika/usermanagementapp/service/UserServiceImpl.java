d38f1445b6faf615b63147ffacc951c9d5985c4b
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-06 07:42:11 +0700

    #12 - Add many implementation in UserServiceImpl

--- src/main/java/com/hendisantika/usermanagementapp/service/UserServiceImpl.java
+++ src/main/java/com/hendisantika/usermanagementapp/service/UserServiceImpl.java
@@ -1,9 +1,15 @@
 package com.hendisantika.usermanagementapp.service;
 
+import com.hendisantika.usermanagementapp.model.User;
 import com.hendisantika.usermanagementapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +28,57 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private PasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public void saveUser(User user) {
+        if (user.getId() == null) {
+            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+            user.setActive(Boolean.TRUE);
+        }
+        userRepository.save(user);
+    }
+
+    @Override
+    public Page<User> listUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<User> searchByTerm(String name, Pageable pageable) {
+        return userRepository.searchByTerm(name, pageable);
+    }
+
+    @Override
+    public Boolean removeAll() {
+        userRepository.deleteAll();
+        return Boolean.TRUE;
+    }
+
+    @Override
+    public void removeById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public List<User> searchBy(String keyword, String criteria) {
+        if ("firstName".equals(criteria)) {
+            return userRepository.findByFirstNameIgnoreCaseContaining(keyword);
+        } else if ("lastName".equals(criteria)) {
+            return userRepository.findByLastNameIgnoreCaseContaining(keyword);
+        } else if ("email".equals(criteria)) {
+            return userRepository.findByEmailIgnoreCaseContaining(keyword);
+        }
+        return new ArrayList<>();
+
+    }
 }
