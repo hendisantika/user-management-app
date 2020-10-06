deeaf4ed39fb7029de11e232468b9ab3fa128dba
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-06 07:48:36 +0700

    #24 - Add home in UserController

--- src/main/java/com/hendisantika/usermanagementapp/controller/UserController.java
+++ src/main/java/com/hendisantika/usermanagementapp/controller/UserController.java
@@ -1,9 +1,25 @@
 package com.hendisantika.usermanagementapp.controller;
 
+import com.hendisantika.usermanagementapp.model.RoleNames;
+import com.hendisantika.usermanagementapp.model.User;
 import com.hendisantika.usermanagementapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +42,31 @@ public class UserController {
     @Value("${max.card.display.on.pagination.tray}")
     private int maxPaginationTraySize;
 
+    @GetMapping("/")
+    public ModelAndView home(@RequestParam(value = "page", defaultValue = "0", required = false) Integer page,
+                             @RequestParam(value = "size", defaultValue = "4", required = false) Integer size,
+                             HttpServletRequest request, HttpServletResponse response) {
+
+        Collection<SimpleGrantedAuthority> authorities = (Collection<SimpleGrantedAuthority>)
+                SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        List<String> list = new ArrayList<>();
+        authorities.forEach(e -> {
+            list.add(e.getAuthority());
+        });
+
+        ModelAndView modelAndView = new ModelAndView();
+        if (list.contains(RoleNames.ADMIN.name())) {
+            modelAndView.setViewName("home");
+            Page<User> allUsers = userService.listUsers(PageRequest.of(page, size, Sort.by("firstName")));
+            modelAndView.addObject("allUsers", allUsers);
+            modelAndView.addObject("maxTraySize", size);
+            modelAndView.addObject("currentPage", page);
+        } else {
+            modelAndView.setViewName("user-home");
+            User user = userService.findUserByEmail(request.getUserPrincipal().getName());
+            modelAndView.addObject("currentUser", user);
+        }
+
+        return modelAndView;
+    }
 }
