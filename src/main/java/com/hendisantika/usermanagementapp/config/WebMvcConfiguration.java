341d4452e8fca5bb50b69335637ea7a02448a16d
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-06 07:47:42 +0700

    #22 - Add WebMvcConfiguration in SecurityConfiguration

---
+++ src/main/java/com/hendisantika/usermanagementapp/config/WebMvcConfiguration.java
@@ -0,0 +1,42 @@
+package com.hendisantika.usermanagementapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/10/20
+ * Time: 07.47
+ */
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+}
