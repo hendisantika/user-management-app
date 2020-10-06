0477269f369055849d3b027eeed3ed9769210051
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-06 07:43:21 +0700

    #14 - Add AppConstant

---
+++ src/main/java/com/hendisantika/usermanagementapp/constant/AppConstant.java
@@ -0,0 +1,16 @@
+package com.hendisantika.usermanagementapp.constant;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/10/20
+ * Time: 07.43
+ */
+public class AppConstant {
+
+    public static final String SUCCESS = "success";
+    public static final String FAIL = "fail";
+}
