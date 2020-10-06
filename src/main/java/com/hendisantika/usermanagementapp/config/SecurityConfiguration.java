dedcc0df75716dd60e6e48b5d33ccbe82645d948
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-06 07:46:04 +0700

    #18 - Add SecurityConfiguration

---
+++ src/main/java/com/hendisantika/usermanagementapp/config/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.hendisantika.usermanagementapp.config;
+
+import com.hendisantika.usermanagementapp.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/10/20
+ * Time: 07.45
+ */
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = CustomUserDetailsService.class)
+@AutoConfigureWebMvc
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordencoder());
+    }
+
+
+}
