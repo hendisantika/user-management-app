44884b775dd84b451c51734dea08d0002be25a5e
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-06 07:46:20 +0700

    #19 - Add passwordencoder in SecurityConfiguration

--- src/main/java/com/hendisantika/usermanagementapp/config/SecurityConfiguration.java
+++ src/main/java/com/hendisantika/usermanagementapp/config/SecurityConfiguration.java
@@ -3,12 +3,15 @@ package com.hendisantika.usermanagementapp.config;
 import com.hendisantika.usermanagementapp.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,6 +34,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordencoder());
+    }
+
+    @Bean(name = "passwordEncoder")
+    public PasswordEncoder passwordencoder() {
+        return new BCryptPasswordEncoder();
     }
 
 
