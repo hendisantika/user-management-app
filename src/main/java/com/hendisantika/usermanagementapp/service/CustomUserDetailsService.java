cf0c29ae52f964f606c70386bacc1c8f936019bd
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-06 07:39:23 +0700

    #8 - Add CustomUserDetailsService

---
+++ src/main/java/com/hendisantika/usermanagementapp/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.hendisantika.usermanagementapp.service;
+
+import com.hendisantika.usermanagementapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/10/20
+ * Time: 07.39
+ */
+@Service("customUserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
