24f30deded1865fab74ec415dea48687fd5f084f
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-06 07:35:52 +0700

    #4 - Add RoleNames Enum

--- src/main/java/com/hendisantika/usermanagementapp/UserManagementAppApplication.java
+++ src/main/java/com/hendisantika/usermanagementapp/UserManagementAppApplication.java
@@ -2,9 +2,16 @@ package com.hendisantika.usermanagementapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserManagementAppApplication {
+public class UserManagementAppApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserManagementAppApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserManagementAppApplication.class, args);
---
+++ src/main/java/com/hendisantika/usermanagementapp/model/RoleNames.java
@@ -0,0 +1,14 @@
+package com.hendisantika.usermanagementapp.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/10/20
+ * Time: 07.35
+ */
+public enum RoleNames {
+    ADMIN, USER
+}
